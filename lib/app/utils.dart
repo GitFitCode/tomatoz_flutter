1f778d1959a6b630fd8dbcb704aff11c165b7f54
Pratik <borolepratik@gmail.com>
2021-02-10 00:24:02 -0800

    first stab at effective logging

--- lib/app/utils.dart
+++ lib/app/utils.dart
@@ -1,2 +1,21 @@
+import 'package:logger/logger.dart';
+
 /// Function to fill with a zero and always show two digits.
 String fill(int n) => n.toString().padLeft(2, "0");
+
+class TomatozLogPrinter extends LogPrinter {
+  final String className;
+
+  TomatozLogPrinter(this.className);
+
+  @override
+  List<String> log(LogEvent event) {
+    AnsiColor color = PrettyPrinter.levelColors[event.level];
+    String emoji = PrettyPrinter.levelEmojis[event.level];
+    return [color('$emoji $className - ${event.message}')];
+  }
+}
+
+Logger getLogger(String className) {
+  return Logger(printer: TomatozLogPrinter(className));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:tomatoz_flutter/app/locator.dart';
 import 'package:tomatoz_flutter/app/router.gr.dart';
+import 'package:tomatoz_flutter/app/utils.dart';
 
 void main() {
+  Logger.level = Level.verbose;
   setupLocator();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final log = getLogger('MyApp');
+
   @override
   Widget build(BuildContext context) {
+    log.d('App Launched');
+
     return MaterialApp(
       theme: ThemeData(
         fontFamily: 'FilsonPro',
